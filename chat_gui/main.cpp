4efc2a90e4dee8608ccca048ad36182e810c6f26
MarchMol <marchmol98@gmail.com>
2025-04-02 11:17:04 -0600

    fix: ui bugs

--- chat_gui/main.cpp
+++ chat_gui/main.cpp
@@ -1,11 +1,8 @@
 #include "mainwindow.h"
 #include <QApplication>
-extern "C" {
-    #include "client.h"
-    }
+
 int main(int argc, char *argv[])
 {
-    hello_from_c();
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
